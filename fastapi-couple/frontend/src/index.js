2044ff3c2261e2f763ea4c548e7bec68195317fc
salamoa-96-Oct <mjsmjh1212@naver.com>
2024-11-18 15:42:52 +0900

    remove

--- fastapi-couple/frontend/src/index.js
+++
@@ -1,7 +0,0 @@
-// project/frontend/src/index.js
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './styles.css'; // CSS 파일 임포트
-
-ReactDOM.render(<App />, document.getElementById('root'));
